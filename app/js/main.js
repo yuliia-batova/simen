92087573f98d00567c89e7a56920a623fd353672
yliia-batova <zayka130192@meta.ua>
2021-04-19 13:26:29 +0300

    adjusted the css code according to the design rules

--- app/js/main.js
+++ app/js/main.js
@@ -5,7 +5,11 @@ $(function () {
   });
   $(".header__search").mouseout(function () {
     $(".header__search").hide();
-    $(".header__menu-inner").show(); 
+    $(".header__menu-inner").show();
+  }); 
+  $(".header__search").mouseout(function () {
+    $(".header__search").hide();
+    $(".header__menu-inner").show();
   });
   $(".intro__slider").slick({
     dots: true,
@@ -36,36 +40,40 @@ $(function () {
     animation: {
       easing: "ease-in-out",
     },
-  }); 
+  });
 
-  $(".popular__slider-items").slick({    
+  $(".popular__slider-items").slick({
     slidesToShow: 4,
     rows: 2,
     slidesToScroll: 8,
-    adaptiveHeight: true,        
+    adaptiveHeight: true,
     arrows: true,
     dots: false,
-    appendArrows: $('.popular__slider-arrows'),
-    prevArrow: '<button class="slick-arrow slick-prew"><span class="icon-long-arrow-left"></span></button>',
-    nextArrow: '<button class="slick-arrow slick-next"><span class="icon-long-arrow-right"></span></button>',
+    appendArrows: $(".popular__slider-arrows"),
+    prevArrow:
+      '<button class="slick-arrow slick-prew"><span class="icon-long-arrow-left"></span></button>',
+    nextArrow:
+      '<button class="slick-arrow slick-next"><span class="icon-long-arrow-right"></span></button>',
   });
-  $(".posts__slider-items").slick({    
-    slidesToShow: 3,    
+  $(".posts__slider-items").slick({
+    slidesToShow: 3,
     slidesToScroll: 1,
-    adaptiveHeight: true,        
+    adaptiveHeight: true,
     arrows: true,
     dots: false,
-    appendArrows: $('.posts__slider-arrows'),
-    prevArrow: '<button class="slick-arrow slick-prew"><span class="icon-long-arrow-left"></span></button>',
-    nextArrow: '<button class="slick-arrow slick-next"><span class="icon-long-arrow-right"></span></button>',
+    appendArrows: $(".posts__slider-arrows"),
+    prevArrow:
+      '<button class="slick-arrow slick-prew"><span class="icon-long-arrow-left"></span></button>',
+    nextArrow:
+      '<button class="slick-arrow slick-next"><span class="icon-long-arrow-right"></span></button>',
   });
-  $(".brands__slider").slick({    
-    slidesToShow: 6,    
+  $(".brands__slider").slick({
+    slidesToShow: 6,
     slidesToScroll: 1,
-    adaptiveHeight: true,        
+    adaptiveHeight: true,
     arrows: false,
     dots: false,
-    autoplay: true, 
-    autoplaySpeed: 2000,   
+    autoplay: true,
+    autoplaySpeed: 2000,
   });
 });
